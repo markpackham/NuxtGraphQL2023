8a1e1178d7ac09fbcdacf191caff68d481a60ae0
mark <markpackham1@gmail.com>
2023-06-20 12:17:56 +0100

    use NuxtPage to exploit pages directory
    https://youtu.be/eXSVyfBLtnY?t=782

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <SiteHeader />
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
+      <NuxtPage />
+    </main>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">Hi I'm Bob</h1>
+        <p class="text-base text-gray-900 p-2 italic">CodeMonkey love coffee cake</p>
+    </section>
+</template>
