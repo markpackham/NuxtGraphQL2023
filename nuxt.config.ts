3bb2c6d7fd4a03cfe0920c8f6d757c0ce3617650
mark <markpackham1@gmail.com>
2023-06-21 10:14:14 +0100

    setup nuxt content
    https://youtu.be/oh2KQqtJLhA?t=174

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss', 'nuxt-icon',
-  ]
+    '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content'
+  ],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  }
 })
