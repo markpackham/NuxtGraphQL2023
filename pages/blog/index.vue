5f903119b185e476b01b12f687b99b7e84e811dd
mark <markpackham1@gmail.com>
2023-06-22 11:40:12 +0100

    add default index page for blog
    https://youtu.be/GXgWQN_NJnA?t=536

---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+const { data: posts } = await useAsyncData('posts', () =>
+    queryContent('/blog').find()
+)
+</script>
+
+<template>
+    <h1 class="text-3xl my-8">Blog Posts</h1>
+
+    I like to write about anything thta I am currently working or something new that interests me. If you would like me to
+    write about something or be a guest blogger on your blog please reach out to me on Twitter. If you would like to
+    subscribe to an RSS feed you can find it here.
+
+    <ul>
+        <li v-for="post in posts" :key="post._id"></li>
+    </ul>
+</template>
