e4e52444bb586a63b69564901fb1845d1c7c4fce
mark <markpackham1@gmail.com>
2023-06-22 10:29:42 +0100

    use ContentRenderer with data
    https://youtu.be/GXgWQN_NJnA?t=302

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,6 +1,16 @@
+<script setup>
+const { path } = useRoute()
+
+const { data } = await useAsyncData(`content-${path}`, () => {
+    return queryContent()
+        .where({ _path: path })
+        .findOne()
+})
+</script>
+
 <template>
-    <!-- <p>{{ $route.params.slug }}</p> -->
-    <main>
-        <ContentDoc />
-    </main>
+    <ContentRenderer :value="data" class="prose my-10 mx-auto max-w-7xl" />
+    <div class="my-8">
+
+    </div>
 </template>
