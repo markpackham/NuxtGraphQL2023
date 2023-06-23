7b49b5c00d1384bfdf4c98186b1409b67b31a608
mark <markpackham1@gmail.com>
2023-06-23 16:16:19 +0100

    use nuxt apollo
    https://youtu.be/Vh357OVNSlo?t=377

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content'
+    '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content', '@nuxtjs/apollo'
   ],
   content: {
     // https://content.nuxtjs.org/api/configuration
