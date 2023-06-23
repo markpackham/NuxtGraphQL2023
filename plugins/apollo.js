b1807e326ebf6669c2e35eb0267018b175f4e18e
mark <markpackham1@gmail.com>
2023-06-23 19:02:45 +0100

    complete authorization to obtain github projects
    https://youtu.be/Vh357OVNSlo?t=1019

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -2,11 +2,7 @@
 
 export default defineNuxtPlugin((nuxtApp) => {
   const { githubToken } = useRuntimeConfig();
-
   nuxtApp.hook("apollo:auth", ({ client, token }) => {
-    // `client` can be used to differentiate logic on a per client basis.
-
-    // apply apollo client token
-    token.value = "<secret_token>";
+    token.value = githubToken;
   });
 });
